03206be05495d30d903ece68151dad47a725845b
SpaceApples <7674856@students.swin.edu.au>
2018-05-21 17:03:08 +1000

    Support classes implemented, forward chainer changed to use support classes

--- src/iengine/Chainer.java
+++ src/iengine/Chainer.java
@@ -1,17 +1,58 @@
 package iengine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Chainer {
+	//contains all variables that are true
+	protected List<Variable> trueVar = new ArrayList<Variable>();
+	//contains a list of all equations that infer
+	protected List<Literal> literals = new ArrayList<Literal>();
+	//contains a list of completed equations that infer
+	protected List<Literal> solvedLiterals = new ArrayList<Literal>();
+	//query to be found
+	protected String query;
 
     // parent class for backwards/forwards chaining
     public Chainer() {}
 
     // output message for when KB has been solved
-    public abstract void solved();
+    protected abstract void solved();
 
     // loops through KB in attempt to prove query with given chaining method
     public abstract void askQuery();
 
     // allocates KB data into appropriate variables for chaining
-    public abstract void interpretKB();
+	public Variable[] initVariable(String[] vars) {
+		Variable[] temp = new Variable[vars.length];
+		for(int v = 0; v < vars.length; v++) {
+			for(int i = 0; i < trueVar.size(); i++) {
+				if(trueVar.get(i).getValue() == vars[v]) {
+					temp[v] = trueVar.get(i);
+				}
+				else {
+					temp[v] = new Variable(vars[v]);
+				}
+			}
+		}
+		return temp;
+	}
+	
+	public void interpretKB(List<String[]> KB){
+		//split the kb 
+		for(int i = KB.size()-1; i > 0; i--){
+			//if array size is 1 it's variable is true
+			if(KB.get(i).length == 1) {
+				Variable temp = initVariable(KB.get(i))[0];
+				temp.setActive(true);
+				trueVar.add(temp);
+			} 
+			else {
+				//else it's an a&b => x
+				Variable[] temp = initVariable(KB.get(i));
+				literals.add(new Literal(temp));
+			}
+		}
+	}
 
 }
--- src/iengine/Literal.java
+++ src/iengine/Literal.java
@@ -1,20 +1,35 @@
 package iengine;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Literal {
 
-    private List<String> implies; // list of variables implying variable
-    private String implied; // implied variable
+    private Variable[] implies; // list of variables implying variable
+    private Variable implied; // implied variable
 
     // ttValue - truth table TBD
 
-    public Literal(String[] vars) {
-        implies = new ArrayList<Variable>();
-        for(int i = 0; i < vars.length-1; i++) {
-            implies.add(vars[i]);
+    public Literal(Variable[] vars) {
+        implies = new Variable[vars.length-1];
+    	for(int i = 0; i < vars.length-1; i++) {
+            implies[i] = vars[i];
         }
-        implied = vars[vars.length-“1];
+        implied = vars[vars.length-1];
+    }
+    
+    public boolean literalTrue() {
+    	for(Variable v : implies) {
+    		if(v.getActive() == false) {
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    public Variable getImplied() {
+    	return implied;
+    }
+    
+    public Variable[] getImplies() {
+    	return implies;
     }
 }
