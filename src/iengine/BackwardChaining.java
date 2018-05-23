4196c63ff59d213b2dd48f152105b9e15d6089c4
tylerbarker <tylercbarker@icloud.com>
2018-05-23 20:24:17 +1000

    finalised BC/implemented main for testing+cmd-line when complete

--- src/iengine/BackwardChaining.java
+++ src/iengine/BackwardChaining.java
@@ -7,15 +7,6 @@ import java.util.LinkedList;
 
 public class BackwardChaining extends Chainer {
 
-//    //contains all variables that are true
-//    protected List<Variable> trueVar = new ArrayList<Variable>();
-//    //contains a list of all equations that infer
-//    protected List<Literal> literals = new ArrayList<Literal>();
-//    //contains a list of completed equations that infer
-//    protected List<Literal> solvedLiterals = new ArrayList<Literal>();
-//    //query to be found
-//    protected String query;
-
     public BackwardChaining(List<String[]> KB, String _query) {
         query = _query;
         interpretKB(KB);
@@ -24,11 +15,17 @@ public class BackwardChaining extends Chainer {
     // output message for when KB has been solved
     @Override
     public void solved(boolean result) {
+        String answer = "";
         if (result) {
             // true result
+            for(int i = provenVars.size() - 1; i >= 0; i--) {
+                answer+= provenVars.get(i) + " ";
+            }
+            System.out.println("YES: " + answer);
         }
         else {
             // false message
+            System.out.println("NO: " + query + " is false.");
         }
     }
 
@@ -38,30 +35,28 @@ public class BackwardChaining extends Chainer {
 
         boolean KBCompleted = false;
         boolean varIsTrue = false;
-        List<String> proven = new ArrayList<>();
+
         Queue<String> toProves = new LinkedList<>(); // Queue of variables to prove
-        toProves.add(query);
+        toProves.add(query); // add query as first in queue
 
         while(toProves.size() > 0) {
-            String toProve = toProves.remove();
-            //System.out.println(literals.size());
+            String toProve = toProves.remove(); // get next variable to prove from queue
             for(int i = literals.size()-1; i >= 0; i--) {
+                // for every literal in list...
                 Literal lit = literals.get(i);
-                String implicitVal = lit.getImpliedValue();
-                Variable[] litImpliers = lit.getImpliers();
+                String implicitVal = lit.getImpliedValue(); // store implied
+                Variable[] litImpliers = lit.getImpliers(); // store impliers
 
-                // if value already known to be true...
+                // if variable already known to be true...
                 if (this.hasTrueVar(toProve)) {
                     varIsTrue = true;
-                    //System.out.println(toProve);
-                    proven.add(toProve); // add proven value to list
+                    provenVars.add(toProve); // add provenVars value to list
                     break;
                 }
                 else {
                     if(implicitVal.equals(toProve)) {
-
                         varIsTrue = true;
-                        proven.add(toProve);
+                        provenVars.add(toProve);
                         // for every implier of given literal...
                         for (int k = 0; k < litImpliers.length; k++) {
                             // add to toProves queue
@@ -72,9 +67,8 @@ public class BackwardChaining extends Chainer {
                 }
             }
         }
+        // print result
         solved(varIsTrue);
-        //for (int i = 0; i < proven.size(); i++)
-        //    System.out.println(proven.get(i));
     }
 }
 
--- src/iengine/Main.java
+++ src/iengine/Main.java
@@ -3,12 +3,39 @@ package iengine;
 public class Main {
 
     public static void main(String[] args) {
-        FileInput file = new FileInput("src/iengine/test.txt");
 
-        //ForwardChaining fc = new ForwardChaining(file.getKB(), file.getQuery());
-        //fc.askQuery();
-
+        // testing methods/input
+        FileInput file = new FileInput("src/iengine/test2.txt");
+        ForwardChaining fc = new ForwardChaining(file.getKB(), file.getQuery());
         BackwardChaining bc = new BackwardChaining(file.getKB(), file.getQuery());
+        //fc.askQuery();
         bc.askQuery();
+
+        // Final Main Implementation
+        //FileInput file = new FileInput(args[1]);
+        //TruthTable tt = new TruthTable(file.getKB(), file.getQuery());
+        //ForwardChaining fc = new ForwardChaining(file.getKB(), file.getQuery());
+        //BackwardChaining bc = new BackwardChaining(file.getKB(), file.getQuery());
+
+        switch (args[0])
+        {
+            case "TT":
+                break;
+
+            case "FC":
+                fc.askQuery();
+                break;
+
+            case "BC":
+                bc.askQuery();
+                break;
+
+            default:
+                System.out.println("Invalid algorithm. Valid algorithms are:");
+                System.out.println("TT");
+                System.out.println("FC");
+                System.out.println("BC");
+                break;
+        }
     }
 }
